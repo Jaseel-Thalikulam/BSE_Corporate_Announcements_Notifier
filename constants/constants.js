011d444ec9ffe03644e2d48ebabb522b40f43bb0
JASEEL T A <jaseelta1@gmail.com>
2024-07-10 16:57:42 +0530

    updated constants

--- constants/constants.js
+++ constants/constants.js
@@ -3,8 +3,6 @@ dotenv.config()
 
 export const secrets = {
   port: process.env.PORT,
-  gmail: process.env.GMAIL,
-  nodemailerAppPassword: process.env.NODEMAILER_APP_PASSWORD,
   telegramBotToken: process.env.TELEGRAM_BOT_TOKEN,
   mongooseUrl: process.env.MONGOOSE_URL,
 };
